9b386d039268589687966aa367286166b49bb177
Mathieu Nicaudie <mathieu.nicaudie@gmail.com>
2017-02-23 15:38:45 +0100

    Base du script

--- script.sh
+++ script.sh
@@ -1,1 +1,7 @@
 #!/bin/bash -e
+
+lxc launch images:debian/jessie/amd64 <nom du conteneur>
+lxc exec <nom du conteneur> -- apt update
+lxc exec <nom du conteneur> -- apt -y upgrade
+lxc exec <nom du conteneur> -- apt -y install openssh-server
+lxc exec <nom du conteneur> -- adduser $USER
